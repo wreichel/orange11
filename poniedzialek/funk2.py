53ec34b1c96a8154f2834e2bd630e71274711c96
Wojtek <wojciech.reichel@gmail.com>
2023-01-11 11:54:01 +0100

    poniedzialek 11:53

--- poniedzialek/funk2.py
+++ poniedzialek/funk2.py
@@ -4,3 +4,10 @@ def sum_numeric(limit = 10) -> int:
         if n % 3 == 0 or n % 5 == 0:
             s += n
     return s
+
+wyniki = []
+wyniki.append(sum_numeric(20))
+wyniki.append(sum_numeric(21))
+wyniki.append(sum_numeric(22))
+wyniki.sort()
+print(wyniki)
