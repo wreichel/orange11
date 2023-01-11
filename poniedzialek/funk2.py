608f2f135f37cdb8a84c3e746baac7bb46fd8986
Wojtek <wojciech.reichel@gmail.com>
2023-01-11 12:03:27 +0100

    poniedzialek 11:53

--- poniedzialek/funk2.py
+++ poniedzialek/funk2.py
@@ -1,3 +1,6 @@
+from collections.abc import Sequence
+
+
 def sum_numeric(limit = 10) -> int:
     s = 0
     for n in range(1, limit):
@@ -5,9 +8,17 @@ def sum_numeric(limit = 10) -> int:
             s += n
     return s
 
+def suma(seq: Sequence[int]) -> int:
+    if len(seq) == 0:
+        return 0
+    return seq[0] + suma(seq[1:])
+
 wyniki = []
 wyniki.append(sum_numeric(20))
 wyniki.append(sum_numeric(21))
 wyniki.append(sum_numeric(22))
 wyniki.sort()
 print(wyniki)
+print(suma(wyniki))
+print(suma([2,3]))
+print(suma([]))
